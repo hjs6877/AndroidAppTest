42ea27b928b42d9a78e8294762c07074ba51066a
hjs6877 <hjs6877@gmail.com>
2015-06-03 15:18:27 +0900

    aa

--- app/src/main/java/siksco/kr/co/androidapptest/LinearLayoutActivity.java
+++ app/src/main/java/siksco/kr/co/androidapptest/LinearLayoutActivity.java
@@ -2,30 +2,23 @@ package siksco.kr.co.androidapptest;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
 
-public class SectionLayoutMainActivity extends ActionBarActivity {
-    public final static String TAG = "SectionLayoutMain";
+public class LinearLayoutActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_section_layout_main);
-    }
-
-
-    public void openExmLinearLayout(View v){
-        Log.i(TAG, "open SectionLayoutMainActivity");
+        setContentView(R.layout.activity_linear_layout);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_section_layout_main, menu);
+        getMenuInflater().inflate(R.menu.menu_linear_layout, menu);
         return true;
     }
 
